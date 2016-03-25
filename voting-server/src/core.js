6accf4025aa20461d05f7af8437175daec493627
sean1rose <sean1rose@gmail.com>
2016-03-25 00:25:41 -0700

    Set reducer logic

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,15 +1,43 @@
 import {List, Map} from 'immutable';
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries){
  // entries is an array
  // 'entries' is the key on state obj
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote){
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else return [a,b];
+}
+
 export function next(state){
-  const entries = state.get('entries');
-  return state.merge({
-    vote: ({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  if (entries.size === 1){
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
+
+}
+
+export function vote(state, entry){
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
 }
